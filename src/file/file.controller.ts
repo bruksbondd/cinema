4fd0148453fd11ce1240f820b49e1ac57d46f239
User <bruksbond@gmail.com>
2024-07-03 13:16:58 +0300

    add swager for upload file

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,7 +2,7 @@ import { Controller, HttpCode, Post, Query, UploadedFile, UseInterceptors } from
 import { FileService } from './file.service';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiBearerAuth('defaultBearerAuth')
 @ApiTags('files')
@@ -11,13 +11,27 @@ export class FileController {
   constructor(private readonly fileService: FileService) {}
 
   @Post()
-  @HttpCode(200)
-  @Auth('admin')
-  @UseInterceptors(FileInterceptor('image'))
-  async uploadFile(
-    @UploadedFile() file: Express.Multer.File,
-    @Query('folder') folder?: string
-  ) {
-    return this.fileService.saveFiles([file], folder)
-  }
+	@HttpCode(200)
+	@Auth('admin')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    required: true,
+    schema: {
+      type: "object",
+      properties: {
+        file: {
+          type: "string",
+          format: "binary",
+        }
+      }
+    }
+  })
+  @ApiQuery({ name: 'folder', required: false, type: String })
+	async uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Query('folder') folder?: string
+	) {
+		return this.fileService.saveFiles([file], folder)
+	}
 }
