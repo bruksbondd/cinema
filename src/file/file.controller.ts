d2f8f8d9f5fdf17036332b9123f5f138dea31afe
User <bruksbond@gmail.com>
2024-07-03 12:44:48 +0300

    add edpoint files

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileService } from './file.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('file')
+@ApiBearerAuth('defaultBearerAuth')
+@ApiTags('files')
+@Controller('files')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
+
+  @Post()
+  @HttpCode(200)
+  @Auth('admin')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Query('folder') folder?: string
+  ) {
+    return this.fileService.saveFiles([file], folder)
+  }
 }
